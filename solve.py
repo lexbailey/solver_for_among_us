5a75a96af1bbdfcc597fd091bdfa68912e24fb93
Daniel Bailey <daniel.bailey@arm.com>
2020-10-08 00:57:01 +0100

    Simplify rules and removed player limit, added rules around remaining impostors

--- solve.py
+++ solve.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 import z3
 
-max_players = 10
-
 class WrongColour(Exception):
     """ Means that this colour isn't in the set of avilable colours """
     pass
@@ -22,9 +20,10 @@ class GameSolver:
     impostor """
     def __init__(self, n_players, n_impostors):
         """ Construct a GameSolver with a known number of players and number of impostors """
+        self.n_players = n_players
         # Some variables that model the state of each player
         self.alive, self.murdered, self.impostor, self.ejected = (
-            [z3.Bool('%s-%d' % (varname, player_id)) for player_id in range(max_players)]
+            [z3.Bool('%s-%d' % (varname, player_id)) for player_id in range(self.n_players)]
             for varname in [
                 'player-is-alive'
                 ,'player-was-murdered'
@@ -35,7 +34,7 @@ class GameSolver:
 
         # First bit of information known is the number of impostors
         self.n_impostors = z3.IntVal(n_impostors)
-        self.n_players = n_players
+        self.n_alive_impostors = z3.Int('alive_impostors')
         self.knowns = [] # for known facts that will never change until the game ends
 
         # State for building facts about players during the game, when what is known
@@ -50,26 +49,22 @@ class GameSolver:
         return [
             # Number of impostors is number of impostors
             self.n_impostors == sumbools(self.impostor)
+            # A number of impostors can be alive
+            ,self.n_alive_impostors == sumbools(z3.And(self.alive[i], self.impostor[i]) for i in range(self.n_players))
             # People that were murdered cannot be impostors
             ,*[
                 z3.Implies(self.murdered[i], z3.Not(self.impostor[i]))
-                for i in range(max_players)
+                for i in range(self.n_players)
             ]
-            # People that were murdered or ejected are not alive
+            # Alive people have not been murdered and have not been ejected
             ,*[
-                z3.Implies(z3.Or(self.murdered[i], self.ejected[i]), z3.Not(self.alive[i]))
-                for i in range(max_players)
+                z3.And(z3.Not(self.murdered[i]), z3.Not(self.ejected[i])) == self.alive[i]
+                for i in range(self.n_players)
             ]
-            # People that dont exist are not alive
-            ,*[
-                z3.Not(self.alive[i])
-                for i in range(max_players) if i > self.n_players-1
-            ]
-            # People that don't exist are not impostors
-            ,*[
-                z3.Not(self.impostor[i])
-                for i in range(max_players) if i > self.n_players-1
-            ]
+            # There is at least one not-dead impostor
+            ,self.n_alive_impostors > z3.IntVal(0)
+            # Twice the number of alive impostors is less than the number of alive crewmates
+            ,sumbools(self.alive) > (self.n_alive_impostors * z3.IntVal(2))
         ]
 
     def get_varying_knowns(self):
@@ -78,12 +73,12 @@ class GameSolver:
             # People that were murdered were murdered
             *[
                 self.murdered[pid] == z3.BoolVal(pid in self.murdered_pids)
-                for pid in range(max_players)
+                for pid in range(self.n_players)
             ]
             # People that were ejected were ejected
             ,*[
                 self.ejected[pid] == z3.BoolVal(pid in self.ejected_pids)
-                for pid in range(max_players)
+                for pid in range(self.n_players)
             ]
         ]
 
@@ -91,15 +86,15 @@ class GameSolver:
         """ Returns a constraint that prevents the given model from being valid """
         return z3.Or(
             *[self.impostor[pid] != model[self.impostor[pid]]
-            for pid in range(max_players)]
+            for pid in range(self.n_players)]
         )
 
     def combine_solutions(self, models):
         """ Generates the count of models in which each player is an impostor from a
         list of models that satisfy the constraints """
-        counts = [0 for _ in range(max_players)]
+        counts = [0 for _ in range(self.n_players)]
         for model in models:
-            for i in range(max_players):
+            for i in range(self.n_players):
                 if z3.is_true(model[self.impostor[i]]):
                     counts[i] += 1
         return counts, len(models)
